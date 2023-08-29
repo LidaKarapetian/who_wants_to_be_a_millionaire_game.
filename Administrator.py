c3e929e90a292cad1ba9df6f4db09303f3304b53
Lida Karapetian <108219180+LidaKarapetian@users.noreply.github.com>
2023-08-29 12:32:31 +0400

    Add files via upload

---
+++ Administrator.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+#Functions to add new questions to the questions.txt file
+
+
+def read_questions_from_file(fname):
+    with open(fname, "r") as file:
+        return file.read()
+    
+def ask_to_add_question_to_file(fname):
+    answer = input("Do you want to add a new question/ (y/n): ")
+    if answer.lower() == "y":
+        question = input("Write a new question! ")
+        
+        with open(fname, "r") as file:
+            initial_questions = file.read()
+            if question in initial_questions:
+                print("Entered question exists in file, please enter another question. ")
+                return
+        
+        correct_answer = input("Enter thw correct answer: ")
+        options = input("Enter the 3 options: ")
+
+        with open(fname, "a") as file:
+            split_options = ",".join(options.split())
+            file.write(f'{question}:?{correct_answer},{split_options}\n')
+    else:
+        print("No new question added: ")
+
+def main():
+    fname = "questions.txt"
+    print(read_questions_from_file(fname))
+    ask_to_add_question_to_file(fname)
+
+main()
+
+
